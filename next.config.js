51e2611f9f5fc05cd159eaf906df6c6df880f4ef
tonnon <lucastonnon@gmail.com>
2023-12-06 22:11:34 -0300

    deploY

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: "/nextjs-page", assetPrefix:"/nextjs-page",
+    
+    
 }
 
 module.exports = nextConfig
