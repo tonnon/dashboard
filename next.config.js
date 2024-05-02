1a83ab99d7b8cb1a6de6cbcda38a873bba0ff076
tonnon <lucastonnon@gmail.com>
2024-05-02 18:10:37 -0300

    deploy

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV = 'production';
-
 const nextConfig = {
     distDir: 'dist',
-    basePath: isProd ? '/dashboard' : '',
+    basePath: '/dashboard',
     images: {
         unoptimized: true,
             remotePatterns: [{
