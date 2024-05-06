560091dadc0aaed1097820df62ac00d890537bd1
tonnon <lucastonnon@gmail.com>
2024-05-06 16:56:35 -0300

    deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+    basePath: '/dashboard',
+    distDir: 'dist',
     images: {
         unoptimized: true,
             remotePatterns: [{
