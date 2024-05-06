24c5f2a26889aedb48548ae2a4a78dbbb462e0ea
tonnon <lucastonnon@gmail.com>
2024-05-06 17:01:54 -0300

    remove auth

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    basePath: '/dashboard',
+    basePath: '/dashboard/dashboard',
     distDir: 'dist',
     images: {
         unoptimized: true,
